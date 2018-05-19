d636cf2acc26e23da50b8d719f39d4111d401960
Tiago Bento <tiagoben@users.noreply.github.com>
2018-05-19 19:29:38 -0300

    Allowing CORS

--- src/main/java/br/com/nx/api/Api.java
+++ src/main/java/br/com/nx/api/Api.java
@@ -1,17 +1,18 @@
 package br.com.nx.api;
 
+import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Api {
-	
-	@RequestMapping("/user")
-	public Object userDetails(OAuth2AuthenticationToken auth) {
-		Map<String, Object> attributes = auth.getPrincipal().getAttributes();
-		return attributes;
+
+	@RequestMapping("/hello")
+	public Object userDetails() {
+		Map<String, Object> hello = new HashMap<>();
+		hello.put("message", "hello world");
+		return hello;
 	}
 }
