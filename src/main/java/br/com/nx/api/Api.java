2db48a365052fe9620ef47b2dd4df68179504879
Tiago Bento <tiagoben@users.noreply.github.com>
2018-05-19 14:59:46 -0300

    OAuth 2.0 set up

--- src/main/java/br/com/nx/api/Api.java
+++ src/main/java/br/com/nx/api/Api.java
@@ -1,19 +1,17 @@
 package br.com.nx.api;
 
-import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Api {
 	
-	@RequestMapping("/api")
-	public Object start() {
-		Map<String, String> start = new HashMap<>();
-		start.put("project", "NX");
-		return start;
+	@RequestMapping("/user")
+	public Object userDetails(OAuth2AuthenticationToken auth) {
+		Map<String, Object> attributes = auth.getPrincipal().getAttributes();
+		return attributes;
 	}
-	
 }
