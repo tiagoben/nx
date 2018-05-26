5b56a3d9c709083ea7ae92de47f707b6d78dadae
Tiago Bento <tiagoben2@gmail.com>
2018-05-26 16:38:05 -0300

    All endpoints are private, except if explicitly declared as public

--- src/main/java/br/com/nx/config/security/OAuth2ServerConfig.java
+++ src/main/java/br/com/nx/config/security/OAuth2ServerConfig.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2002-2013 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package br.com.nx.config.security;
 
 import javax.sql.DataSource;
@@ -52,9 +37,10 @@ public class OAuth2ServerConfig {
 
 		@Override
 		public void configure(HttpSecurity http) throws Exception {
-			http.requestMatchers().antMatchers("/produtos/**", "/oauth/users/**", "/oauth/clients/**", "/user")
-				.and()
-				.authorizeRequests().antMatchers("/user").access("#oauth2.hasScope('read')");
+	        http
+	          .authorizeRequests()
+	              .antMatchers("/", "/hello", "/public/**", "/actuator/health").permitAll()
+	              .anyRequest().authenticated();
 		}
 	}
 
--- src/main/java/br/com/nx/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/nx/config/security/WebSecurityConfig.java
@@ -5,12 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -38,23 +36,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
     
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-                 http
-            .authorizeRequests()
-                .antMatchers("/", "/login").permitAll()
-                .anyRequest().hasRole("USER")
-                .and()
-            // TODO: put CSRF protection back into this endpoint
-            .csrf()
-                .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize"))
-                .disable()
-            .logout()
-            	.logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .and()
-            .formLogin();
-        // @formatter:on
-    }
 }
